82a3bd4892c95090bcd0aabb041d598a900b0195
Khor De Long <khor.delong@openmove.ai>
2026-05-30 15:35:20 +0800

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/example/loan_spring/LoanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loan_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
